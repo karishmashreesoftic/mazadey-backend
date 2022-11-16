87b3fb9b99b325df99c7340c231522dbbfe07e1b
appnwebdeveloper7 <appnwebdeveloper7@gmial.com>
2022-11-16 10:16:46 +0530

    uploadqid

--- utils/multer.js
+++ utils/multer.js
@@ -1,22 +1,22 @@
 const multer = require("multer")
 const {GridFsStorage} = require('multer-gridfs-storage');
 
-const storage = new GridFsStorage({
-    url: process.env.DB_URI,
-    file: (req, file) => {
-      console.log("multer...file...",file)
-      return new Promise((resolve, reject) => {
-          const filename = file.originalname;
-          const fileInfo = {
-            filename: filename,
-            bucketName: "uploads"
-          };
-          resolve(fileInfo);
-      });
-    }
-  });
+// const storage = new GridFsStorage({
+//     url: process.env.DB_URI,
+//     file: (req, file) => {
+//       console.log("multer...file...",file)
+//       return new Promise((resolve, reject) => {
+//           const filename = file.originalname;
+//           const fileInfo = {
+//             filename: filename,
+//             bucketName: "uploads"
+//           };
+//           resolve(fileInfo);
+//       });
+//     }
+//   });
 
-// const storage = new GridFsStorage({ url : process.env.DB_URI})
+const storage = new GridFsStorage({ url : process.env.DB_URI})
 const upload = multer({storage});
 
 module.exports = upload
