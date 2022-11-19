3e8e45033a05db2661018eea3257cf5a0f03e625
appnwebdeveloper7 <appnwebdeveloper7@gmial.com>
2022-11-19 11:59:28 +0530

    wishlist for seller

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -4,15 +4,9 @@ const Member = require('../models/Member')
 exports.addToWishlist = async(req,res) => {
     try{
 
-        if(req.member.membertype==="customer"){
-
-            const newWishlist = req.member.wishlist.concat(req.params.id)
-            await Member.findByIdAndUpdate(req.member._id, {wishlist: newWishlist})
-            res.status(200).send({message: "Success"}) 
-
-        }else{
-            throw new Error("Only customers are allowed to perform this action.")
-        }
+        const newWishlist = req.member.wishlist.concat(req.params.id)
+        await Member.findByIdAndUpdate(req.member._id, {wishlist: newWishlist})
+        res.status(200).send({message: "Success"}) 
 
     }catch(error){
         res.send({message: error.message})
@@ -22,15 +16,9 @@ exports.addToWishlist = async(req,res) => {
 exports.removeFromWishlist = async(req,res) => {
     try{
 
-        if(req.member.membertype==="customer"){
-
-            const newWishlist = req.member.wishlist.filter((i)=> {return i.toString() !== req.params.id})
-            await Member.findByIdAndUpdate(req.member._id, {wishlist: newWishlist})
-            res.status(200).send({message: "Success"}) 
-
-        }else{
-            throw new Error("Only customers are allowed to perform this action.")
-        }
+        const newWishlist = req.member.wishlist.filter((i)=> {return i.toString() !== req.params.id})
+        await Member.findByIdAndUpdate(req.member._id, {wishlist: newWishlist})
+        res.status(200).send({message: "Success"}) 
 
     }catch(error){
         res.send({message: error.message})
@@ -39,15 +27,9 @@ exports.removeFromWishlist = async(req,res) => {
 
 exports.getWishlist = async(req, res) => {
     try{
-
-        if(req.member.membertype==="customer"){
-            
-            const w = await Member.findById(req.member._id).populate('wishlist','title photos type').select('wishlist')
-            res.status(201).send(w.wishlist)
-
-        }else{
-            throw new Error("Only customers are allowed to perform this action.")
-        }
+  
+        const w = await Member.findById(req.member._id).populate('wishlist','title photos type').select('wishlist')
+        res.status(201).send(w.wishlist)
 
     }catch(error){
         res.send({message: error.message})
